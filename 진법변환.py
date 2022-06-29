0b8992f74fc082cc8c960723e020f5a213f35dbe
HongChan Yoon <hongchani0324@gmail.com>
2022-06-29 23:49:17 +0900

    Algorithm Practice

---
+++ 진법변환.py
@@ -0,0 +1,49 @@
+# 문제
+# B진법 수 N이 주어진다. 이 수를 10진법으로 바꿔 출력하는 프로그램을 작성하시오.
+
+# 10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다. 이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+
+# A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
+
+# 입력
+# 첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36)
+
+# B진법 수 N을 10진법으로 바꾸면, 항상 10억보다 작거나 같다.
+
+# 출력
+# 첫째 줄에 B진법 수 N을 10진법으로 출력한다.
+
+# 예제 입력 1 
+# ZZZZZ 36
+
+# 예제 출력 1 
+# 60466175
+
+### 풀이 ###
+
+lst = list(input())
+
+B = int(input())
+
+def solution(ls, b):
+
+    new_lst = []
+    cnt=1
+    answer = 0
+
+    for i in ls:
+        if ord(i) <= 57:
+            new_lst.append(ord(i)-48)
+        elif ord(i) > 57:
+            new_lst.append(ord(i)-55)
+
+    print(new_lst)
+    
+    for i in new_lst:
+        answer += i*pow(b, (len(new_lst)-cnt))
+        cnt += 1
+
+    return answer
+
+
+print(solution(lst, B))
